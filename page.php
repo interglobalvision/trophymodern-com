5239e70fad9771d008183162c4d51c48d7bcc386
Patrick Best <patrickbest@patrickbest.com>
2015-10-13 13:30:46 -0500

    add inquiries form markup to page template

--- page.php
+++ page.php
@@ -25,6 +25,20 @@ if( have_posts() ) {
 
       <?php the_content(); ?>
 
+      <form id="form-inquiries" class="font-color-black">
+
+        <h4 class="font-color-red u-align-center">Inquiries?</h4>
+
+        <input type="text" name="name" placeholder="name" />
+
+        <input required type="email" name="email" placeholder="email" />
+
+        <textarea name="message" rows="7" placeholder="message"></textarea>
+
+        <button class="u-pointer" type="submit">Send</button>
+
+      </form>
+
     </article>
 
   </section>
