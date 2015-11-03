d74600a140435a99302fe92e5027a3992e1e6064
Patrick Best <patrickbest@patrickbest.com>
2015-11-03 17:58:29 -0600

    add /exhibition archive template

---
+++ archive.php
@@ -0,0 +1,65 @@
+<?php
+get_header();
+?>
+
+<!-- main content -->
+
+<main id="main-content">
+
+  <section id="exhibitions" class="font-color-white">
+
+<?php
+if( have_posts() ) {
+  while( have_posts() ) {
+    the_post();
+    $meta = get_post_meta($post->ID);
+?>
+
+    <article <?php post_class(); ?>>
+      <header class="exhibition-header u-cf">
+        <div class="percent-col into-2 pcol-padding-right">
+          <h4><?php the_time('F Y'); ?></h4>
+        </div>
+        <div class="percent-col into-2">
+          <h4 class="exhibition-title"><?php the_title(); ?></h4>
+        </div>
+      </header>
+
+      <?php
+        if (has_post_thumbnail()) {
+      ?>
+        <div class="row">
+          <div class="percent-col into-2 pcol-padding-right">
+            <?php the_post_thumbnail('exhibition-single'); ?>
+          </div>
+          <div class="percent-col into-2">
+            <?php the_content(); ?>
+          </div>
+        </div>
+      <?php
+        } else {
+      ?>
+          <?php the_content(); ?>
+      <?php
+        }
+      ?>
+
+    </article>
+
+<?php
+  }
+} else {
+?>
+    <article class="u-alert"><?php _e('Sorry, no posts matched your criteria :{'); ?></article>
+<?php
+} ?>
+
+  </section>
+
+<!-- end main-content -->
+
+</main>
+
+<?php
+get_footer();
+?>
--- functions.php
+++ functions.php
@@ -36,7 +36,7 @@ if( function_exists( 'add_image_size' ) ) {
   add_image_size( 'admin-thumb', 150, 150, false );
   add_image_size( 'opengraph', 1200, 630, true );
 
-  add_image_size( 'name', 199, 299, true );
+  add_image_size( 'exhibition-single', 1000, 1000, false );
 }
 
 // Register Nav Menus
